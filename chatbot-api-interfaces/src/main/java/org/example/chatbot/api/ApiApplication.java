bfc7560d07eb21346600ad107fb3fd292b6657c6
huyifan001 <2692006779@qq.com>
2025-03-06 21:13:07 +0800

    info:初始化

---
+++ chatbot-api-interfaces/src/main/java/org/example/chatbot/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package org.example.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h3>chatbot-api</h3>
+ * <p>ApiApplication</p>
+ *
+ * @author : hyf
+ * @date : 2025-03-06 20:40
+ **/
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+
+
+
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/org/example/chatbot/api/test/ApiTest.java
@@ -0,0 +1,11 @@
+package org.example.chatbot.api.test;
+
+/**
+ * <h3>chatbot-api</h3>
+ * <p>ApiTest</p>
+ *
+ * @author : hyf
+ * @date : 2025-03-06 21:05
+ **/
+public class ApiTest {
+}
